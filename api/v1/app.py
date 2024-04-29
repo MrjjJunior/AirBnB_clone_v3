183017a14034c6ac4f51c75ae1feadea2f7fa73c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-04-29 12:55:25 +0200

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,10 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
