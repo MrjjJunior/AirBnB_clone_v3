454e9a34b72ef28e3bb6830f2c3d6555403e3924
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-01 23:57:59 +0200

    making giles to start Flask application

---
+++ api/v1/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Flask application '''
+from apiv1.views import app_views
+from Flask import flask
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/')
+def app_views():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
