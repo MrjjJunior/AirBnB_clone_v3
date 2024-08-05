e60b608577925fa7d635ae4db502fbc11a356c0a
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-05 10:11:44 +0200

    teardown

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def close_db(error):
+    ''' closes database storage '''
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' 404 error response '''
+    retrun make_response(jsonify({'error': "Not found"}), 404)
+
+
 if __name__ == "__main__":
     HOST = getenv()
     PORT = int(getenv())
