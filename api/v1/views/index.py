702aed30523beabddcae88d003d0378d3607952c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-05 11:38:23 +0200

    setup sql database and index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Flask app '''
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def api_status():
+    ''' '''
+    response = {"status": "OK"}
+    return jsonify(response)
