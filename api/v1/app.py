3aca326456a9369d52d93b3e6a89a62daba134df
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-05 08:41:07 +0200

    trying to resolv git conflicts

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -17,3 +18,19 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.00')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
+=======
+''' Flask application '''
+from apiv1.views import app_views
+from Flask import flask
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/')
+def app_views():
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+>>>>>>> main
