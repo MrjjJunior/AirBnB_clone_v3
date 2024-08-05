6add5003f397761a19fd70773e9d4ec54644bdfd
Tshepiso <tlhongtshepiso2@gmail.com>
2024-08-05 09:15:56 +0200

    resolving from VScode

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 #!/usr/bin/python3
-<<<<<<< HEAD
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -21,20 +17,3 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.00')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
-=======
-''' Flask application '''
-from apiv1.views import app_views
-from Flask import flask
-from models import storage
-
-app = Flask(__name__)
-
-@app.route('/')
-def app_views():
-    pass
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
->>>>>>> main
->>>>>>> Tshepiso
